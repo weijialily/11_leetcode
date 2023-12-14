3cdfb2ed2545ba3bba6d2a1ff44beeb9dbebac2b
weijialily <weijialily@gmail.com>
2023-12-14 01:11:14 -0800

    Built venv

--- 00001_TwoSum.py
+++ 00001_TwoSum.py
@@ -3,12 +3,18 @@ https://leetcode.com/problems/two-sum/description/
 
 """
 
-# # 1. Brute Force 
-# # Time complexity O(n^2)
-# # Space complexity O(1)
-# class Solution:
-#     def twoSum(self, nums: List[int], target: int) -> List[int]:
-#         for i in range(len(nums)):
-#             for j in range(i + 1, len(nums)):
-#                 if nums[j] + nums[i] == target:
-#                     return [i, j]
+# 1. Brute Force 
+# Time complexity O(n^2)
+# Space complexity O(1)
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[j] + nums[i] == target:
+                    return [i, j]
+                
+
+nums = [2,7,11,15]
+target = 9
+output = Solution()
+print(output.twoSum(nums, target))
