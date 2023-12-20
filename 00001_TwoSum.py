bedd9f8ad5a78da1caeae3d92ff63a6503367920
weijialily <weijialily@gmail.com>
2023-12-20 15:43:26 -0800

    Reduce time complexity

--- 00001_TwoSum.py
+++ 00001_TwoSum.py
@@ -3,15 +3,25 @@ https://leetcode.com/problems/two-sum/description/
 """
 
 
-# 1. Brute Force 
-# Time complexity O(n^2)
-# Space complexity O(1)
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[j] + nums[i] == target:
-                    return [i, j]
+        # # 1. Brute Force
+        # # Time complexity O(n^2)
+        # # Space complexity O(1)
+        # for i in range(len(nums)):
+        #     for j in range(i + 1, len(nums)):
+        #         if nums[j] + nums[i] == target:
+        #             return [i, j]
+
+        # 2. One-pass Hash Table
+        # Time complexity O(n)
+        # Space complexity O(n)
+        hashmap = {}
+        for idx in range(len(nums)):
+            res = target - nums[idx]
+            if res in hashmap:
+                return [idx, hashmap[res]]
+            hashmap[nums[idx]] = idx
 
 
 nums = [2, 7, 11, 15]
