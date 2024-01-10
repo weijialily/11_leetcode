5bce018ba1b88cf544959bd18759cbe5b6019f97
weijialily <weijialily@gmail.com>
2024-01-09 16:23:46 -0800

    Leetcode fast hashmap solution

--- 00170_TwoSumIII.py
+++ 00170_TwoSumIII.py
@@ -14,24 +14,72 @@ class TwoSum:
 
     def add(self, number: int):
         """
-        Add the number to existing data structure, and mantain the ascending order
+        Add the number to existing data structure, 
+        and mantain the ascending order.
         """
         self.nums.append(number)
         self.nums.sort()
 
+    # # Two pointers
+    # def find(self, value: int) -> bool:
+    #     idx_low, idx_high = 0, len(self.nums)-1
+    #     while idx_low < idx_high:
+    #         twosum = self.nums[idx_low] + self.nums[idx_high]
+    #         if twosum > value:
+    #             idx_high -= 1
+    #         elif twosum < value:
+    #             idx_low += 1
+    #         else:  # twosum == value
+    #             return True
+    #     return False
+     
+    # Hashmap
     def find(self, value: int) -> bool:
-        idx_low, idx_high = 0, len(self.nums)-1
-        while idx_low < idx_high:
-            twosum = self.nums[idx_low] + self.nums[idx_high]
-            if twosum > value:
-                idx_high -= 1
-            elif twosum < value:
-                idx_low += 1
-            else: # twosum == value
+        hashmap = {}
+        for idx in range(len(self.nums)):
+            res = value - self.nums[idx]
+            if res in hashmap:
                 return True
-
+            else:
+                hashmap[self.nums[idx]] = idx
         return False
 
+# Hashmap solution beats 95.64% Leetcode users  
+# class TwoSum(object):
+
+#     def __init__(self):
+#         """
+#         Initialize your data structure here.
+#         """
+#         self.num_counts = {}
+
+
+#     def add(self, number):
+#         """
+#         Add the number to an internal data structure..
+#         :type number: int
+#         :rtype: None
+#         """
+#         if number in self.num_counts:
+#             self.num_counts[number] += 1
+#         else:
+#             self.num_counts[number] = 1
+
+#     def find(self, value):
+#         """
+#         Find if there exists any pair of numbers which sum is equal to the value.
+#         :type value: int
+#         :rtype: bool
+#         """
+#         for num in self.num_counts.keys():
+#             comple = value - num
+#             if num != comple:
+#                 if comple in self.num_counts:
+#                     return True
+#             elif self.num_counts[num] > 1:
+#                 return True
+        
+#         return False
 
 # Your TwoSum object will be instantiated and called as such:
 obj = TwoSum()
