8a00e1323ca0fbcbbd6bf81f0f2476e3f726cf14
weijialily <weijialily@gmail.com>
2024-04-30 21:15:40 -0700

    sql 579, 595, 601

--- sql/00569_MedianEmployeeSalary.sql
+++ sql/00569_MedianEmployeeSalary.sql
@@ -21,3 +21,12 @@ FROM add_rank r
 JOIN add_count c
 ON r.company = c.company
 AND r.rnk BETWEEN c.cnt/2 AND c.cnt/2+1
+-- # Write your MySQL query statement below
+SELECT e.id, e.month,
+    SUM(e2.Salary) as Salary
+FROM Employee e
+JOIN Employee e2 
+ON (e.Id = e2.Id AND e.Month >= e2.Month AND (e.Month - e2.Month <= 2))
+WHERE e.Month < (SELECT MAX(Month) from Employee where Id = e.Id)
+GROUP BY e.Id, e.Month
+ORDER BY e.Id, e.Month DESC
